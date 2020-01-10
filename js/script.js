881c6c91180547e37684468ff80f09026814d582
Ostap <43958957+pawas007@users.noreply.github.com>
2020-01-10 16:46:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function onYouTubePlayerAPIReady() {
+    var playerYoutube;
+
+    playerYoutube = new YT.Player("video-youtube__content", {
+        videoId: "cxXUEDbOxgo",
+        events: {
+            onReady: onYouTubePlayerReady
+        }
+    });
+}
+
+function onYouTubePlayerReady(event) {
+    // https://developers.google.com/youtube/iframe_api_reference#Events
+    var targetYoutubeVideo = event.target;
+    var videoDomElem = document.getElementById(
+        event.target.getIframe().getAttribute("id")
+    );
+    var newPlayBtn = videoDomElem.nextElementSibling;
+
+    newPlayBtn.addEventListener("click", function(event) {
+        targetYoutubeVideo.playVideo();
+        this.classList.add('hidden');
+        videoDomElem.classList.remove('video-youtube__content_hide-origin-play-btn');
+        videoDomElem.parentNode.classList.remove('video-youtube_overlay');
+    });
+}
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+/* -------------------------------------------------------------------------
+   end Video Youtube
+ * ------------------------------------------------------------------------- */
